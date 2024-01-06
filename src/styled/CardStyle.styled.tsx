9c19c3786dafdd47ae5833c9ce7cccc68e6caa45
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-07 02:23:59 +0300

    1

--- src/styled/CardStyle.styled.tsx
+++ src/styled/CardStyle.styled.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 
 export const CardStyle = styled.article`
-  width: 600px;
+  width: 300px;
+  height: 350px;
   background-color: #7c4e4e;
+  border-radius: 15px;
 `  
