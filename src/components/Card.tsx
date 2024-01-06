e57d09d8d693863ee4f726e808acd23d8aa03866
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-07 01:01:52 +0300

    new

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,12 @@
+import React from "react"
+import { CardStyle } from "../styled/CardStyle.styled"
+
 function Card () {
-  <div>Hello</div>
+  return (
+    <div>
+      <CardStyle>Привет</CardStyle>
+    </div>
+  )
 }
+
+export default Card
---
+++ src/styled/CardStyle.styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const CardStyle = styled.article`
+  width: 600px;
+  background-color: #7c4e4e;
+`  
