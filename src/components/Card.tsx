3f55e0c4b634bfa2a76bf2389ead2742779017e5
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-08 04:00:35 +0300

    1

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { CardStyle } from "../styled/CardStyle.styled"
+import CardText from "./CardText"
+import CardContent from "./CardContent"
 
 function Card () {
   return (
-    <CardStyle>
-      <CardContent></CardContent>
-    </CardStyle>
-  )
+		<CardStyle>
+      <CardContent />
+		</CardStyle>
+	)
 }
 
 export default Card
--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -1,8 +1,26 @@
 import CardText from "./CardText";
+import { ReactComponent as image } from "../image/Rectangle 1.svg"
+import { CardImage } from "./CardImage";
+
 
 function CardContent () {
+  let image = 
+  /* Rectangle 1 */
+
+console.log(test)
+
   return(
-    <CardText></CardText>  
-    <CardImage></CardImage>
+    <>
+      <CardImage src="image"></CardImage>
+      {/* <CardText>
+        Headline
+      </CardText>
+      <CardText>
+        Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
+        ornare in venen.
+      </CardText> */}
+    </>
   )
 }
+
+export default CardContent;
