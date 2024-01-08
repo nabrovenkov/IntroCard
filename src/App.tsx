d9b0764a28b15d74ad81bd30296d642cbe6a1cf2
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-08 23:06:04 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
 		<div className='App'>
 			<CardWrapper>
-        <div>"dfsdf"</div>
 				<Card />
 			</CardWrapper>
 		</div>
--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -6,13 +6,20 @@ import { CardImage } from "./CardImage";
 function CardContent (props: any) {
 
 
-  return(
-    <>
-      <CardImage>
-        <Image />
-      </CardImage>
-    </>
-  )
+  return (
+		<>
+			<CardImage>
+				<Image />
+			</CardImage>
+			<CardText TextView={"title"}>
+        "Headline"
+      </CardText>
+			<CardText TextView={"text"}>
+				"Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
+				ornare in venen."
+			</CardText>
+		</>
+	)
 }
 
 export default CardContent;
